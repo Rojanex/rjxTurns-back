234ae72f6ad9a3e935a29989765f009375a419b0
Lu <77848575+Rojanex@users.noreply.github.com>
2023-09-07 12:46:10 -0500

    get local ip

--- run.py
+++ run.py
@@ -5,11 +5,14 @@ import urllib3
 urllib3.disable_warnings()
 
 from app import create_app
-from flask_socketio import SocketIO
+import socket
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.7.38")
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    host = s.getsockname()[0]
+    app.run(debug=True, host=host)
     #http_server = WSGIServer(('', 5000), app)
     #http_server.serve_forever()
